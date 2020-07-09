22359de8fe2a9dbc0960e01474b17b5a5c8a1237
BooGeon <qnrjs42@gmail.com>
2020-07-10 01:30:21 +0900

    2-9 // 회원가입 페이지 만들기(커스텀 훅)

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState, useCallback, useMemo } from 'react'
 import { Form, Input, Button } from 'antd'
 import Link from 'next/link'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import useInput from '../hooks/useInput'
 
 const ButtonWrapper = styled.div `
     margin-top: 10px;
@@ -11,43 +13,47 @@ const FormWrapper = styled(Form)`
     padding: 10px
 `;
 
-const LgoinForm = ({ setIsLoggedIn }) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+const LoginForm = ({ setIsLoggedIn }) => {
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
 
-    const onChangePassword = useCallback((e) => {
-      setPassword(e.target.value);
-    }, []);
+    setIsLoggedIn(true);
+  }, [id, password]);
 
-    const onSubmitForm = useCallback(() => {
-        console.log(id, password);
-
-        setIsLoggedIn(true);
-        
-    }, [id, password]);
-
-
-
-    return (
-      <FormWrapper onFinish={onSubmitForm}>
-        <div>
-          <label htmlFor="user-id">아이디</label> <br />
-          <Input name="user-id" value={id} onChange={onChangeId} required />
-        </div>
-        <div>
-          <label htmlFor="user-password">비밀번호</label> <br />
-          <Input name="user-password" value={password} onChange={onChangePassword} required />
-        </div>
-        <ButtonWrapper>
-            <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-        </ButtonWrapper>
-      </FormWrapper>
-    );
+  return (
+    <FormWrapper onFinish={onSubmitForm}>
+      <div>
+        <label htmlFor="user-id">아이디</label> <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label> <br />
+        <Input
+          name="user-password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <ButtonWrapper>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </ButtonWrapper>
+    </FormWrapper>
+  );
 };
 
-export default LgoinForm;
+LoginForm.propTypes = {
+    setIsLoggedIn: PropTypes.func.isRequired,
+}
+
+export default LoginForm;
