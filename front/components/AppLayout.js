f7438bb0e989334717eb7dd467d47723b1413e78
BooGeon <qnrjs42@gmail.com>
2020-07-09 22:29:38 +0900

    2-4 // 로그인 폼 만들기

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { Menu, Input, Row, Col } from 'antd'
 
+import UserProfile from '../components/UserProfile'
+import LoginForm from '../components/LoginForm'
+
 const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -39,15 +43,22 @@ const AppLayout = ({ children }) => {
           */}
 
         <Col xs={24} md={6}>
-            왼쪽 메뉴
+          { isLoggedIn ? <UserProfile /> : <LoginForm /> }
         </Col>
 
         <Col xs={24} md={12}>
           {children}
         </Col>
-        
+
         <Col xs={24} md={6}>
-            <a href="https://www.naver.com" target="_blank" rel="noreferrer noopner">Made by Naver</a>
+          {/* target="_blank" rel="noreferrer noopner" rel 해당 속성은 _balnk 보안 위협을 제거해줌  */}
+          <a
+            href="https://www.naver.com"
+            target="_blank"
+            rel="noreferrer noopner"
+          >
+            Made by Naver
+          </a>
         </Col>
       </Row>
     </div>
