37ea52f3330a9349edb0765c9162e1cda7671441
BooGeon <qnrjs42@gmail.com>
2020-07-14 12:30:46 +0900

    3-6 // 더미 데이터와 포스트 폼 만들기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 import AppLayout from '../components/AppLayout'
+import PostForm from '../components/PostForm'
+import PostCard from '../components/PostCard'
 
 
 
 const Home = () => {
+    const { isLoggedIn } = useSelector((state) => state.user)
+    const { mainPosts } = useSelector((state) => state.post)
     return (
-       <AppLayout> 
-           <div>Hell, Next!</div> 
-       </AppLayout>
-    )
+      <AppLayout>
+        {isLoggedIn && <PostForm />}
+
+        {mainPosts.map((post, index) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </AppLayout>
+    );
 }
 export default Home;
