7ecb79d03085fe7337c0ab68d7439abb145c1b0b
BooGeon <qnrjs42@gmail.com>
2020-07-16 12:03:58 +0900

    3-10 // 이미지 캐루셀 구현하기(react-slik)

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -3,26 +3,47 @@ import { PlusOutlined } from "@ant-design/icons";
 
 import PropTypes from 'prop-types'
 
+import ImagesZoom from './ImagesZoom'
+
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false)
+
     const onZoom = useCallback(() => {
             setShowImagesZoom(true)
     },[])
+
+    const onClose = useCallback(() => {
+            setShowImagesZoom(false)
+    }, [])
 
     if(images.length === 1) {
         return (
             <>
                 <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         )
     }
     if(images.length === 2) {
         return (
-            <>
-                <img role="presentation" style={{ width: '50%', display: 'inline-block '}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
-                <img role="presentation" style={{ width: '50%', display: 'inline-block '}} src={images[1].src} alt={images[1].src} onClick={onZoom} />
-            </>
-        )
+          <>
+            <img
+              role="presentation"
+              style={{ width: "50%", display: "inline-block " }}
+              src={images[0].src}
+              alt={images[0].src}
+              onClick={onZoom}
+            />
+            <img
+              role="presentation"
+              style={{ width: "50%", display: "inline-block " }}
+              src={images[1].src}
+              alt={images[1].src}
+              onClick={onZoom}
+            />
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+          </>
+        );
     }
     return (
       <>
@@ -50,6 +71,7 @@ const PostImages = ({ images }) => {
             개의 사진 더보기
           </div>
         </div>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
 }
