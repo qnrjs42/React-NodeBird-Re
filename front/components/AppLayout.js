be3822e45c3a3da1f5f84e428a735d393d57c8a6
qnrjs42 <qnrjs42@naver.com>
2020-09-08 19:12:17 +0900

    ch2-3 // 반응형 그리드 사용하기

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -19,14 +19,33 @@ const AppLayout = ({ children }) => {
             </Link>
           </Menu.Item>
           <Menu.Item>
+            <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+          </Menu.Item>
+          <Menu.Item>
             <Link href="/signup">
               <a>회원가입</a>
             </Link>
           </Menu.Item>
         </Menu>
+        {/* gutter란 Column 사이에 간격을 조금 넓혀주는것 */}
+        <Row gutter={8}>
+          <Col xs={24} md={6}>
+            왼쪽 메뉴
+          </Col>
+          <Col xs={24} md={12}>
+            {children}
+          </Col>
+          <Col xs={24} md={6}>
+            <a
+              href="https://www.naver.com"
+              target="_blank"
+              rel="noreferrer noopner"
+            >
+              Made by Naver
+            </a>
+          </Col>
+        </Row>
       </div>
-
-      {children}
     </>
   );
 };
