f248e051ef65d8cea1ca05250b95ea2013bb8fcb
qnrjs42 <qnrjs42@naver.com>
2020-09-09 16:53:02 +0900

    ch3-6 // 더미 데이터와 포스트 폼 만들기

---
+++ front/components/PostForm.js
@@ -0,0 +1,60 @@
+import React, { useCallback, useState, useRef } from "react";
+import { Form, Input, Button } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { addPost } from "../reducers/post";
+
+const PostForm = () => {
+  const { imagePaths } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  const [text, setText] = useState("");
+  const imageInput = useRef();
+
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  // 버튼을 눌러서 사진 업로드 창을 띄울 수 있다
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
+
+  const onSubmit = useCallback(() => {
+    dispatch(addPost);
+    setText("");
+  }, []);
+
+  return (
+    <Form
+      style={{ margin: "10px 0 20px" }}
+      encType="multipart/form-data"
+      onFinish={onSubmit}
+    >
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLgength={140}
+        placeholder="어떤 신기한 일이 있나요?"
+      />
+      <div>
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        <Button type="primary" style={{ float: "right" }} htmlType="submit">
+          짹짹
+        </Button>
+      </div>
+      <div>
+        {imagePaths.map((v) => (
+          <div key={v} style={{ display: "inline-block" }}>
+            <img src={v} style={{ width: "200px" }} alt={v} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
+
+export default PostForm;
--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,17 +1,60 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "제로초",
+      },
+      content: "첫 번째 게시글 #해시태그 #익스프레스",
+      Images: [
+        {
+          src: "",
+        },
+        {
+          src: "",
+        },
+        {
+          src: "",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "hero",
+          },
+          content: "얼른 사고싶어요~",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: "더미데이터입니다.",
+  User: {
+    id: 1,
+    nickname: "제로초",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN":
+    case "ADD_POST":
       return {
         ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
+        mainPosts: [dummyPost, ...state.mainPosts], // 게시글 쓰자마자 제일 위에 보이기 위해 dummyPost를 첫 번재 파라미터에 쓴다
+        postAdded: true,
       };
 
     default:
