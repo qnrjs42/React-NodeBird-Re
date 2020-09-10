736c8921e82ffdf609bfbe00ab0d1c24cbddb3b8
qnrjs42 <qnrjs42@naver.com>
2020-09-10 22:00:37 +0900

    ch4-13 // 팔로우, 언팔로우 구현하기

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,13 +1,21 @@
 import { all, fork, call, put, takeLatest, delay } from "redux-saga/effects";
 import axios from "axios";
 import {
+  LOG_IN_REQUEST,
   LOG_IN_SUCCESS,
   LOG_IN_FAILURE,
+  LOG_OUT_REQUEST,
   LOG_OUT_SUCCESS,
   LOG_OUT_FAILURE,
-  LOG_IN_REQUEST,
-  LOG_OUT_REQUEST,
   SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+  FOLLOW_REQUEST,
+  FOLLOW_SUCCESS,
+  FOLLOW_FAILURE,
+  UNFOLLOW_REQUEST,
+  UNFOLLOW_SUCCESS,
+  UNFOLLOW_FAILURE,
 } from "../reducers/user";
 
 function logInAPI(data) {
@@ -79,6 +87,62 @@ function* signUp() {
   }
 }
 
+function followAPI() {
+  return axios.post("/api/follow");
+}
+
+function* follow(action) {
+  try {
+    // const result = yield call(followAPI);
+    // 성공 결과: result.data
+    // 실패 결과: err.response.data
+
+    yield delay(1000);
+
+    yield put({
+      type: FOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: FOLLOW_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
+function unFollowAPI() {
+  return axios.post("/api/unFollow");
+}
+
+function* unFollow(action) {
+  try {
+    // const result = yield call(unFollowAPI);
+    // 성공 결과: result.data
+    // 실패 결과: err.response.data
+
+    yield delay(1000);
+
+    yield put({
+      type: UNFOLLOW_SUCCESS,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: UNFOLLOW_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchFollow() {
+  yield takeLatest(FOLLOW_REQUEST, follow);
+}
+
+function* watchUnFollow() {
+  yield takeLatest(UNFOLLOW_REQUEST, unFollow);
+}
+
 function* watchLogIn() {
   yield takeLatest(LOG_IN_REQUEST, logIn);
 }
@@ -92,5 +156,11 @@ function* watchSignUp() {
 }
 
 export default function* userSaga() {
-  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
+  yield all([
+    fork(watchFollow),
+    fork(watchUnFollow),
+    fork(watchLogIn),
+    fork(watchLogOut),
+    fork(watchSignUp),
+  ]);
 }
