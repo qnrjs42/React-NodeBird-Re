57dd2ae23ac95038cc3f4553a5100d5095f6de51
qnrjs42 <qnrjs42@naver.com>
2020-09-10 15:27:17 +0900

    ch4-6 // 액션과 상태 정리하기

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,5 +1,14 @@
 import { all, fork, call, put, takeLatest, delay } from "redux-saga/effects";
 import axios from "axios";
+import {
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_FAILURE,
+  LOG_IN_REQUEST,
+  LOG_OUT_REQUEST,
+  SIGN_UP_REQUEST,
+} from "../reducers/user";
 
 function logInAPI(data) {
   return axios.post("/api/login", data);
@@ -13,12 +22,12 @@ function* logIn(action) {
     yield delay(1000);
 
     yield put({
-      type: "LOG_IN_SUCCESS",
+      type: LOG_IN_SUCCESS,
       data: action.data,
     });
   } catch (err) {
     yield put({
-      type: "LOG_IN_FAILURE",
+      type: LOG_IN_FAILURE,
       data: err.response.data,
     });
   }
@@ -37,24 +46,51 @@ function* logOut() {
     yield delay(1000);
 
     yield put({
-      type: "LOG_OUT_SUCCESS",
+      type: LOG_OUT_SUCCESS,
     });
   } catch (err) {
     yield put({
-      type: "LOG_OUT_FAILURE",
+      type: LOG_OUT_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
+function signUpAPI() {
+  return axios.post("/api/signUp");
+}
+
+function* signUp() {
+  try {
+    // const result = yield call(signUpAPI);
+    // 성공 결과: result.data
+    // 실패 결과: err.response.data
+
+    yield delay(1000);
+
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
       data: err.response.data,
     });
   }
 }
 
 function* watchLogIn() {
-  yield takeLatest("LOG_IN_REQUEST", logIn);
+  yield takeLatest(LOG_IN_REQUEST, logIn);
 }
 
 function* watchLogOut() {
-  yield takeLatest("LOG_OUT_REQUEST", logOut);
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
-  yield all([fork(watchLogIn), fork(watchLogOut)]);
+  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchSignUp)]);
 }
