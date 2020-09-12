dd0d4e3903ddb029a94ec577adf3bb27bd4285af
qnrjs42 <qnrjs42@naver.com>
2020-09-12 11:18:08 +0900

    ch5-1 // 노드로 서버 구동하기

---
+++ back/app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.write("Hello node");
+  res.end("Hello Node");
+});
+server.listen(3065, () => {
+  console.log("서버 실행 중");
+});
