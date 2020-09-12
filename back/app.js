e876a9d3c1028a1d558a44b18dd7ac3b4ae6e7a9
qnrjs42 <qnrjs42@naver.com>
2020-09-12 11:45:52 +0900

    ch5-2 // 익스프레스로 라우팅하기

--- back/app.js
+++ back/app.js
@@ -1,10 +1,45 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write("Hello node");
-  res.end("Hello Node");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello express");
 });
-server.listen(3065, () => {
+
+app.get("/api", (req, res) => {
+  res.send("hello api");
+});
+
+app.get("/api/posts", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      content: "hello",
+    },
+    {
+      id: 2,
+      content: "hello2",
+    },
+    {
+      id: 3,
+      content: "hello3",
+    },
+  ]);
+});
+
+app.post("/api/post", (req, res) => {
+  res.json({
+    id: 1,
+    content: "hello",
+  });
+});
+
+app.delete("/api/post", (req, res) => {
+  res.json({
+    id: 1,
+  });
+});
+
+app.listen(3065, () => {
   console.log("서버 실행 중");
 });
